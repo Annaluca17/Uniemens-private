caa29d39b5cc509f52836f969d205766e303ab0d
Annaluca17 <lucamartino1985@gmail.com>
2026-05-07 17:03:10 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import UniEmensBuilder from './UniEmensBuilder.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <UniEmensBuilder />
-  </StrictMode>
-)
